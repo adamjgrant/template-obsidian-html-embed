4a58a5d51b488d968ee08f865c5e96ad42509418
AG <a@b.com>
2024-03-15 21:58:29 -0700

    Good enough for now

--- emoji-game/play.js
+++ emoji-game/play.js
@@ -35,14 +35,16 @@ for (let round = 1; round<6; round++) {
 // Start showing the game
 spots.round_1_1.innerText = game_emojis[0][0];
 spots.round_1_2.innerText = game_emojis[0][1];
+active_round = 1;
 
 const guesses = Array.from(document.querySelectorAll(".guess input"))
 guesses.forEach((guess_element) => {
-  const width_unit = guess_element.getBoundingClientRect().width;
+  const width_unit = guess_element.getBoundingClientRect().width - 5;
   guess_element.addEventListener("keyup", (e) => {
     e.stopPropagation();
     const length = e.target.value.length;
-    const new_width = width_unit * length;
+    const new_width = width_unit * length + 5;
     e.target.style.width = `${new_width}px`;
   })
 })
+
